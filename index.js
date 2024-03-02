b280379341c8a16ae49e20f804e967a65188d012
Star <starvoltz173gmail.com>
2024-03-02 11:01:09 +0530

    Health Check added

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const app = express()
 
 const port = 3000
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/health', (req, res) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        Timestamp: Date.now()
+    };
+
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        res.status(503).send();
+    }
 })
 
 app.listen(port, () => {
